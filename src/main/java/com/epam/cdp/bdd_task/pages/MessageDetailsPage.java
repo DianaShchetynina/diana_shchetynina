8f23270c78a7353fa66f38988abe2c7b5e40985c
Diana_Shchetynina <diana_shchetynina@epam.com>
2017-07-05 18:09:18 +0300

    new load

--- src/main/java/com/epam/cdp/bdd_task/pages/MessageDetailsPage.java
+++ src/main/java/com/epam/cdp/bdd_task/pages/MessageDetailsPage.java
@@ -2,7 +2,6 @@ package com.epam.cdp.bdd_task.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class MessageDetailsPage extends BasePage {
     private static final By messageBodyLocator = By.cssSelector("div[dir=\"ltr\"]");
@@ -10,10 +9,6 @@ public class MessageDetailsPage extends BasePage {
 
     public MessageDetailsPage(WebDriver webDriver) {
         super(webDriver);
-    }
-
-    public String getMessageBody() {
-        return findElement(messageBodyLocator).getText();
     }
 
     public String getMessageSubject() {
